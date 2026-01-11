6f8d27a4711bf8001053a3b7b170e3af3377bae2
Baptista-Paulo-Joaquim <baptistajoaquim0@gmail.com>
2026-01-11 14:17:49 +0200

    feat: removes slash from server.ts file

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ app.use("/uploads", express.static("uploads"));
 
 app.use(
   cors({
-    origin: "https://identificamz.vercel.app/",
+    origin: "https://identificamz.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
