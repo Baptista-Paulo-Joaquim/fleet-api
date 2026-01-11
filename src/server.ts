740fa1b716ffc03a8dc66b2223d5c2994dfbd1e5
Baptista-Paulo-Joaquim <baptistajoaquim0@gmail.com>
2026-01-11 13:42:27 +0200

    feat: setting up cors to connect with frontend

--- src/server.ts
+++ src/server.ts
@@ -11,16 +11,25 @@ dotenv.config();
 const app = express();
 
 app.use("/uploads", express.static("uploads"));
-//app.use(cors({ origin: "https://informa-mz.vercel.app/" }));
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://identificamz.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan("dev"));
